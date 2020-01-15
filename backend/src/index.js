859966cd03d06e8bed116f760f85c392cdea4372
Jean Souza <jeanhrsouza@hotmail.com>
2020-01-15 14:34:45 -0300

    Building the web interface

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes')
+
+const app = express();
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-xpwyy.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
-const app = express();
-
+app.use(cors());
 app.use(express.json()); //precisa vir antes das rotas (routes)
 app.use(routes);
 
